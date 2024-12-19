e5dc07a6f52d9609d1da16ff21f9be2e012a637a
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2024-12-19 12:59:34 +0900

    Time: 3 ms (53.27%), Space: 58.5 MB (88.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    let buyPrice =prices[0];
     let maxProfit = 0 ;
-
-    for(let i = prices.length-1 ; i >= 1  ; i--){
-        for(let j = i -1 ; j>=0;j --){
-            if(prices[j]>=prices[i]){
-                break;
-            }
-            
-            if(prices[i] - prices[j]>maxProfit){
-                maxProfit = prices[i] - prices[j];
-            }
+    
+    for(let i = 1 ;i<prices.length; i++){
+        if(prices[i]< buyPrice){
+            buyPrice = prices[i]
         }
-
+        maxProfit = Math.max(maxProfit,prices[i] - buyPrice)
     }
 
     return maxProfit
@@ -31,4 +26,6 @@ var maxProfit = function(prices) {
 // 젤 간단하게 구현 : 이중반복 돌면서 최대 이윤 저장 -> 시간초과
 // 해시테이블(Map)에 저장해서 가능한 이윤의 최대값이 가능한지(큰 값의 index가 더 높은지) 확인 -> 흠...
 // 아니면 거꾸로 확인? 나중값이 더 커야하니까
-// 거꾸로 확인하면서 앞에 있는 값이 더 크면 break -> 긴 테스트arr 시간초과
+// 거꾸로 확인하면서 앞에 있는 값이 더 크면 break -> 긴 테스트arr 시간초과 -> break조건을 >=로 변화
+
+//이중 순회를 돌 필요없음 -> 이윤 최대값만 저장하면 됨
