92d21dc01475d1ceb338dcf797c25df45889dbcd
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2024-12-20 15:45:39 +0900

    Time: 30 ms (94.37%), Space: 65.3 MB (77.17%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,116 +2,46 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-function areSetsEqual(set1, set2) {
-    if (set1.size !== set2.size) return false;
-    for (let item of set1) {
-        if (!set2.has(item)) return false;
-    }
-    return true;
-}
 
-var threeSum = function(nums) {
-    const positiveNums= new Map()
-    const positiveSet = new Set()
-    const negativeNums= new Map()
-    const negativeSet = new Set()
-    const zero=[]
-    const resultArr=[]
 
-    nums.forEach((num)=>{
-        if(num === 0) return zero.push(num)
-        if(num > 0) { 
-            if(positiveNums.has(num)){
-                 positiveNums.set(num,2) 
-            }else{
-                positiveNums.set(num,1)
-                positiveSet.add(num)
-            }
-            return
+var threeSum = function (nums) {
+    const resultArr = []
+    nums.sort((a, b) => a - b)
+
+    for (let i = 0; i < nums.length; i++) {
+        //중복제거
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
         }
-        if(num < 0){ 
-            if(negativeNums.has(num)){
-                 negativeNums.set(num,2) 
-            }else{
-                negativeNums.set(num,1)
-                negativeSet.add(num)
-            }
-            return
-        } 
-    })
-    console.log(negativeNums,zero,positiveNums)
-    //0이 3개 이상 있는 경우
-    if(zero.length>=3) resultArr.push([0,0,0])
 
-    //0이 1개 이상 있는 경우 -> 양수 + 음수 해서 0만들어야함 -> 양수배열,음수배열에서 절대값같은 숫자 찾음
-    if(zero.length >=1){
-        //중복 확인
-        let tempArr=[]
-        positiveNums.forEach((value,num)=>{
-            if(negativeNums.has(-num) && !tempArr.includes(num)){
-                resultArr.push([-num,0,num])
-                tempArr.push(num)
-            }
-        })
-    }
+        let j = i + 1;
+        let k = nums.length - 1;
+        //j가 k보다 커지는거 방지
+        while (j < k) {
+            let total = nums[i] + nums[j] + nums[k]
 
-    //0이 없을때 -> 양수 + 음수 + 양/음수 해서 0을 만들어야함
-    let tempPositiveArr=[] //
-    
-    //⚠️ map에서 forEach돌때 key값을 나중에 받음
-    for(const positiveNum of positiveSet){
-        let tempNegativeArr=[]
-        for(const negativeNum of negativeSet){
-            let complement = - (positiveNum + negativeNum)
-            if(complement === 0 )  continue;
-            if(tempPositiveArr.includes(complement) || tempNegativeArr.includes(complement)) continue;
-            if(complement < 0) {
-              
-                 //아...부호주의
-                if(negativeSet.has(complement)){
+            if (total > 0) {
+                k--;
+            } else if (total < 0) {
+                j++;
+            } else {
                
-                    if(negativeNum === complement && negativeNums.get(complement) === 1 ){
-                        continue;
-                    }
+                resultArr.push([nums[i], nums[j], nums[k]])
+                j++
 
-                    resultArr.push([negativeNum,complement,positiveNum])
-                    tempNegativeArr.push(negativeNum,complement)
-                    continue;
+                //중복방지
+                while (nums[j] === nums[j - 1] && j < k) {
+                    j++;
                 }
             }
-            if(complement > 0){
-               
-                if(positiveSet.has(complement)){
-                 
-                    if(positiveNum === complement && positiveNums.get(complement) ===1 ){
-                        continue;
-                    }
-
-                    resultArr.push([negativeNum,complement,positiveNum])
-                    tempNegativeArr.push(negativeNum)
-                    continue;
-                }
-            }
-        } 
-        tempPositiveArr.push(positiveNum)
-        
+        }
     }
-
     return resultArr
 
 };
 
-// 주어진 배열에서 합이 0이되는 3개의 숫자를 반환
-// 같은 index의 숫자를 중복해서 사용 불가
-// 3개의 숫자를 골라야한다고? numsC3 개의 경우의 수가 있음
-// 먼저 2개의 숫자를 골라서 더함 -> 나머지 중에 합이 0이 되는 숫자가 있는지 확인
-// index도 확인해야되니까 -> hash table이용?(Map) -> 중복된 숫자는 처리못함
-// 0이 될라면  0.0,0 이 아닌이상 무조건 양수와 음수 둘다 있어야함 -> 양수와 음수를 나눠서 저장?
-
-// 기준 나누기
-// 1) 0 이 3개있는경우
-// 2) 0 이 2개 있는경우 -> 불가능 -> 0으느 홀수개/아예없음 만 가능
-// 3) 0 이 1개 있는 경우
-// 4) 0 이 없는 경우
-
-//시간초과! 루프를 너무 많이 돔
+//배열을 정렬 -> 0개수로 기준나누기와 비슷한 역할
+// [000], [음수,0,양수], [음수, 양/음수, 양수] 
+//포인터 이용 i,j,k -> 최소,최대값 포함, j는 최소 +1
+//만약 다 더했는데 양수 -> 양수값을 줄여야함 -> k 포인터 이동
+// 만약 다 더했는데 음수 -> 음수값을 줄여야함 -> j포인터 이동 -> j>k가 되면 i를 증가
