aeb93b6c1370693b728063c02322a8f123e9d53e
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2024-12-22 17:10:45 +0900

    Time: 12 ms (38.09%), Space: 58.5 MB (63.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -2,22 +2,23 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
-    intervals.sort(([start1,end1],[start2,end3])=> start1-start2)
+var merge = function (intervals) {
+    intervals.sort(([start1, end1], [start2, end3]) => start1 - start2)
 
-    const resultArr=[]
+    const resultArr = []
+    let currentInterval = intervals[0]
 
-    let i = 1
-    resultArr.push(intervals[0])
-    while(i<intervals.length){
-        if(resultArr[resultArr.length-1][1] < intervals[i][0]){
-            resultArr.push(intervals[i])
-        }else{ 
-            Math.max(resultArr[resultArr.length-1][1],intervals[i][1])
-            resultArr[resultArr.length-1][1] = Math.max(resultArr[resultArr.length-1][1],intervals[i][1])
+    for (let i = 1; i < intervals.length; i++) {
+        if (currentInterval[1] < intervals[i][0]) {
+            resultArr.push(currentInterval)
+            currentInterval = intervals[i]
+        } else {
+            currentInterval[1] = Math.max(currentInterval[1], intervals[i][1])
         }
-        i+=1;
     }
-
+    resultArr.push(currentInterval)
     return resultArr
 };
+
+//배열의 마지막요소에 접근 반복하는게 성능을 저하시킴
+//while 보다는 for문?
