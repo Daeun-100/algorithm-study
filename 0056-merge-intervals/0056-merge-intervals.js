f44d4e0436f7e54b3ea26ac98fc6b57a5d28971a
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2024-12-22 17:12:17 +0900

    Time: 10 ms (51.2%), Space: 58.5 MB (63.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -8,11 +8,11 @@ var merge = function (intervals) {
     let currentInterval = intervals[0]
 
     for (let i = 1; i < intervals.length; i++) {
-        if (currentInterval[1] < intervals[i][0]) {
+        if (currentInterval[1] >= intervals[i][0]) {
+            currentInterval[1] = Math.max(currentInterval[1], intervals[i][1])
+        } else {
             resultArr.push(currentInterval)
             currentInterval = intervals[i]
-        } else {
-            currentInterval[1] = Math.max(currentInterval[1], intervals[i][1])
         }
     }
     resultArr.push(currentInterval)
