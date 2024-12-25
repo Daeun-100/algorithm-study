98b14199bcfb5b45c1b1ab1bb7fed131365739a2
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2024-12-25 16:15:50 +0900

    Time: 1 ms (89.61%), Space: 50.3 MB (57.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,23 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const map = new Map()
-    for(let i = 0 ; i < nums.length ; i ++){
-        map.set(nums[i],i);
-    }
+var twoSum = function (nums, target) {
+    const seen = {} // 중복이 되는 map
 
-    for(let i = 0 ; i < nums.length ; i ++){
-        const curNumber = nums[i];
-        const complement = target - curNumber;
-        if(map.has(complement) && map.get(complement) !== i ){
-            return [i,map.get(complement)];
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (seen.hasOwnProperty(complement)) {
+            return [seen[complement], i]
         }
+        seen[nums[i]] = i
     }
-};
 
-// 이중 반복은 시간복잡도 O(n)
-// 시간복잡도를 줄일 수 있는 방법은 없을까? -> 반복문을 하나 줄여야함 -> 숫자와 index를 저장하는 다른 저장소 필요 -> Map
-// 공간복잡도/ 이중 반복은 시간복잡도 O(n)
-// 시간복잡도를 줄일 수 있는 방법은 없을까? -> 반복문을 하나 줄여야함 -> 숫자와 index를 저장하는 다른 저장소 필요 -> Map
-// 공간복잡도는 O(n) 으로 늘어남
+};
