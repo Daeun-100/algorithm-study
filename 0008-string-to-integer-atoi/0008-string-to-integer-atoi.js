ce291bdc051801de0ebcd4599ac3ebcf31aa10b8
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-07 23:16:38 +0900

    Time: 5 ms (20.74%), Space: 54.1 MB (29.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -6,9 +6,8 @@ var myAtoi = function (s) {
     let result = ""
     //앞에 공백 제거
     let i = 0;
-    while (s[i] === " ") {
-        i += 1;
-    }
+    while (s[i] === " ") i += 1;
+
     if (s[i] === "-" || s[i] === "+") {
         result += s[i]
         i += 1;
@@ -16,14 +15,13 @@ var myAtoi = function (s) {
     while (s[i] === "0") {
         i += 1;
     }
-    while (!Number.isNaN(Number(s[i]))&&s[i]!==" ") {
-
+    while (!Number.isNaN(Number(s[i])) && s[i] !== " ") {
         result += s[i]
         i += 1;
     }
-    if(Number.isNaN(Number(result))) return 0
-    if(Number(result)<-(2**31)) return -(2**31)
-    if(Number(result)>2**31-1) return 2**31-1
+    if (Number.isNaN(Number(result))) return 0
+    if (Number(result) < -(2 ** 31)) return -(2 ** 31)
+    if (Number(result) > 2 ** 31 - 1) return 2 ** 31 - 1
 
     return Number(result)
 
