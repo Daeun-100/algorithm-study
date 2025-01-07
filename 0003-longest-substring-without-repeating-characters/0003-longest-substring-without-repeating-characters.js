def6cf4b8558c518ece4c4464d747a8427e8185c
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-07 20:34:27 +0900

    Time: 8 ms (81.05%), Space: 55.2 MB (51.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,25 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let result = 0;
-    let length = 0;
-    let startIndex = 0;
-    const map = new Map()
-    for (let i = 0; i < s.length; i++) {
-        console.log(s[i],length, result)
-        if (!map.has(s[i])) {
-            map.set(s[i], i)
-            length += 1;
-            result = Math.max(result, length)
-            continue
+    let left = 0;
+    let maxLength = 0;
+    let charSet = new Set();
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
         }
-        if (i - map.get(s[i]) > result || i - map.get(s[i]) > length ) {
-            length += 1;
-            result = Math.max(result, length)
-        }else{
-            length = i - map.get(s[i]) 
-        }
-        map.set(s[i], i)
+
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
     }
-    return result
+
+    return maxLength;
 };
