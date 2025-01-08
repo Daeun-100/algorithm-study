5a8f412e5dec0a7a137ef8ab9b0aaea8a03a1f1a
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-08 23:25:04 +0900

    Time: 27 ms (54.3%), Space: 55.3 MB (63.64%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -4,57 +4,41 @@
  * @return {number[]}
  */
 var findAnagrams = function (s, p) {
-    const pMap = new Map()
-    const result = []
 
-    for (let i = 0; i < p.length; i++) {
-        if (pMap.has(p[i])) {
-            pMap.set(p[i], pMap.get(p[i]) + 1)
-        } else {
-            pMap.set(p[i], 1)
+    const output = [];
+    const neededChars = {};
+
+    for (let char of p) {
+        if (char in neededChars) {
+            neededChars[char]++;
+        } else neededChars[char] = 1;
+    }
+
+    let left = 0;
+    let right = 0;
+    let count = p.length;
+    
+    while (right < s.length) {
+        // `s[right]` 문자가 `neededChars`에 있고, 현재 필요한 경우 `count`를 감소시킵니다.
+        if (neededChars[s[right]] > 0) count--;
+
+        // 현재 문자의 필요 개수를 감소시키고 오른쪽 포인터를 한 칸 이동합니다.
+        neededChars[s[right]]--;
+        right++;
+
+        // `count`가 0이 되면, 이는 현재 윈도우에서 애너그램을 찾았다는 의미이므로 `left`를 결과 배열에 추가합니다.
+        if (count === 0) output.push(left);
+
+        // 윈도우의 길이가 `p`의 길이에 도달하면 윈도우를 한 칸 이동시키기 위해 왼쪽 포인터를 증가시킵니다.
+        if (right - left == p.length) {
+            // 왼쪽에서 제외되는 문자가 필요한 문자라면, `count`를 증가시킵니다.
+            if (neededChars[s[left]] >= 0) count++;
+
+            // 윈도우에서 제외된 문자를 다시 필요 목록(`neededChars`)에 추가하여, 이후에 다시 사용할 수 있도록 복원합니다.
+            neededChars[s[left]]++;
+            left++;
         }
     }
-    console.log(pMap)
-    let prev;
-    let next;
-    for (let i = 0; i < s.length; i++) {
-        console.log(i)
-        if (prev && next && prev === next) {
-            result.push(i)
-            prev = s[i]
-            next = s[i + p.length]
-            continue;
-        }
-        let sMap = new Map()
-        let isBreak = false
-        for (let j = 0; j < p.length; j++) {
-            if (!pMap.has(s[i + j])) {
-                i = i + j
-                isBreak = true
-                break;
-            }
-            if (sMap.has(s[i + j])) {
-                sMap.set(s[i + j], sMap.get(s[i + j]) + 1)
-            } else {
-                sMap.set(s[i + j], 1)
-            }
-            console.log(sMap)
-        }
-        if (isBreak) {
-            prev = null
-            next = null
-            continue;
-        }
-
-        if (JSON.stringify([...sMap.entries()].sort((a, b) => a[0].localeCompare(b[0]))) === JSON.stringify([...pMap.entries()].sort((a, b) => a[0].localeCompare(b[0])))) {
-          
-            result.push(i)
-            prev = s[i]
-            next = s[i + p.length]
-        }
-    }
-    return result
-
+    // 모든 애너그램의 시작 인덱스를 반환합니다.
+    return output;
 };
-//각 index에서 p의 길이만큼 반복을 해서 각 문자가 1번씩 포함되는지 확인해야함
-// p에 없는 문자면 그다음 index부터 다시 검사
