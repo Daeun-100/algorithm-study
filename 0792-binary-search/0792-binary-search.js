130ef0005e651b060b07477eef750ec9b0e6c814
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-11 14:13:29 +0900

    Time: 6 ms (4.96%), Space: 52.4 MB (86.06%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length
+    let index = Math.floor(nums.length / 2)
+    const set = new Set()
+    do {
+        console.log(nums[index])
+        if (nums[index] === target) return index
+        if (nums[index] < target) {
+            set.add(nums[index])
+            left = index
+            index = Math.floor((left + right) / 2)
+        } else if (nums[index] > target) {
+            set.add(nums[index])
+            right = index
+            index = Math.floor((left + right) / 2)
+        }
+    } while (!set.has(nums[index]))
+
+    return -1
+};
