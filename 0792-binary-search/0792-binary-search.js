f30c06bfd86d95b455ffa87b77577732784546d3
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-11 14:16:50 +0900

    Time: 0 ms (100%), Space: 51.9 MB (97.47%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -5,22 +5,17 @@
  */
 var search = function (nums, target) {
     let left = 0;
-    let right = nums.length
-    let index = Math.floor(nums.length / 2)
-    const set = new Set()
-    do {
-       
+    let right = nums.length - 1
+
+    while (left <= right) {
+        let index = Math.floor((left + right) / 2)
         if (nums[index] === target) return index
         if (nums[index] < target) {
-            set.add(nums[index])
-            left = index
-            index = Math.floor((left + right) / 2)
+            left = index+1
         } else if (nums[index] > target) {
-            set.add(nums[index])
-            right = index
-            index = Math.floor((left + right) / 2)
+            right = index-1
         }
-    } while (!set.has(nums[index]))
+    }
 
     return -1
 };
