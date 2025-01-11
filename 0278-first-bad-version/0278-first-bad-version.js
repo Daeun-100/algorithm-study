ae7e1c9b0a2dc5f0dc99cd225557215699fec7ce
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-11 15:01:13 +0900

    Time: 41 ms (96.29%), Space: 48.9 MB (47.15%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -19,17 +19,17 @@ var solution = function (isBadVersion) {
      */
     return function (n) {
         let begin = 1;
-        let end = n 
-        while (begin <= end) {
-            let current = Math.floor((begin + end) / 2) 
+        let end = n
+        while (begin < end) {
+            let current = Math.floor((begin + end) / 2)
             if (!isBadVersion(current)) {
                 begin = current + 1
             } else {
                 //true인 경우 젤 낮은 true값 찾아야함
-                if (current===1 || !isBadVersion(current-1)  ) return current
-                end = current - 1
+                end = current
             }
         }
+        return begin
 
 
     };
