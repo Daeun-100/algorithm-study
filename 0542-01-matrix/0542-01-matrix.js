287d0568fbb02a0e6ea06e1703758e885c248a4c
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-16 17:49:54 +0900

    Time: 7 ms (98.88%), Space: 57 MB (98.51%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -2,49 +2,24 @@
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var updateMatrix = function (mat) {
-    let start = []
-    const rows = mat.length;
-    const cols = mat[0].length;
-    let output = mat.map((arr, i) => arr.map((num, j) => {
-        if (num === 0) {
-            start.push([i, j])
-            return 0
-        }
-        return Infinity
-    }))
-
-    let queue = start
-
-    const directions = [
-        [-1, 0], // 위
-        [1, 0],  // 아래
-        [0, -1], // 왼쪽
-        [0, 1]   // 오른쪽
-    ];
-
-    while (queue.length > 0) {
-        let [r, c] = queue.shift()
-        //string을 만들고 set으로 확인하는 작업이 오래걸림
-        // let string = i + "," + j
-   
-
-        for (const [dr, dc] of directions) {
-            const newRow = r + dr;
-            const newCol = c + dc;
-
-            // 유효한 범위 확인 및 거리 갱신
-            if (
-                newRow >= 0 && newRow < rows &&
-                newCol >= 0 && newCol < cols &&
-                output[newRow][newCol] > output[r][c] + 1
-            ) {
-                output[newRow][newCol] = output[r][c] + 1;
-                queue.push([newRow, newCol]);
+var updateMatrix = function(mat) {
+    let highest = mat.length + mat[0].length;
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[0].length; j++) {
+            if (mat[i][j] === 0) {
+                continue;
             }
+            mat[i][j] = Math.min(i > 0 ? mat[i - 1][j] : highest, j > 0 ? mat[i][j - 1] : highest) + 1;
         }
-
     }
-    return output
 
+    for (let i = mat.length - 1; i >= 0; i--) {
+        for (let j = mat[0].length - 1; j >=0; j--) {
+            if (mat[i][j] === 0) {
+                continue;
+            }
+            mat[i][j] = Math.min(i + 1 < mat.length ? mat[i + 1][j] + 1 : highest, j + 1 < mat[0].length ? mat[i][j + 1] + 1 : highest, mat[i][j]);
+        }
+    }
+    return mat;
 };
