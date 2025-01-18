110a35232cb93d325dcfe7e550edc21944fde37a
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-01-18 17:42:33 +0900

    Time: 53 ms (89.61%), Space: 51.3 MB (87.02%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -13,30 +13,21 @@
 var cloneGraph = function (node) {
     if (node === null) return null
     let stack = [node]
-    let newNodes = {}
-    let set = new Set()
-    while (stack.length > 0) {
+    let visited = new Map()
+    visited.set(node, new Node(node.val));
 
+    while (stack.length > 0) {
         let node = stack.pop()
 
-        if (set.has(node.val)) continue
-        set.add(node.val)
-        if (!newNodes[node.val]) {
-            const newNode = new Node(node.val)
-            newNodes[node.val] = newNode
-        }
+        for (const neighbor of node.neighbors) {
+            if (!visited.has(neighbor)) {
+                visited.set(neighbor, new Node(neighbor.val));
+                stack.push(neighbor); // 스택에 추가
+            }
+            visited.get(node).neighbors.push(visited.get(neighbor))
 
-        for (let i = node.neighbors.length - 1; i >= 0; i--) {
-            if (!newNodes[node.neighbors[i].val]) {
-                const newNode = new Node(node.neighbors[i].val)
-                newNodes[node.neighbors[i].val] = newNode
-            }
-            newNodes[node.val].neighbors.push(newNodes[node.neighbors[i].val])
-            if (!set.has(node.neighbors[i].val)) {
-                stack.push(node.neighbors[i])
-            }
         }
     }
-    
-    return newNodes[1]
+
+    return visited.get(node)
 };
