ebf0c1ebe1f83c3f5a12f37dcce622a0d15724ac
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-02-01 14:20:00 +0900

    Time: 64 ms (86.5%), Space: 58.3 MB (81.33%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const searchParents = (val, root, parents) => {
+    parents.push(root.val)
+    if (val === root.val) return
+    else if (val < root.val) root = root.left
+    else if (val > root.val) root = root.right
+
+    searchParents(val, root, parents)
+}
+
+const findLCA = (val, root, parents, answer) => {
+
+    if (parents.includes(root.val)) {
+        answer.push(root)
+    }
+    if (val === root.val) return
+    else if (val < root.val) root = root.left
+    else if (val > root.val) root = root.right
+
+    findLCA(val, root, parents, answer)
+
+}
+
+var lowestCommonAncestor = function (root, p, q) {
+    let parents = []
+    searchParents(p.val, root, parents)
+
+    let answer = []
+    findLCA(q.val, root, parents, answer)
+ 
+    return answer.at(-1)
+};
+  //공통부모는 무조건 존재함
