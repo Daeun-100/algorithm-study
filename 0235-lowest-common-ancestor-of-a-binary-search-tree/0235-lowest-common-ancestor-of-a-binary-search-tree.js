0fd7928686d49f5ae75d65c863f74a706cfb0049
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-02-01 14:28:43 +0900

    Time: 73 ms (47.83%), Space: 59.1 MB (46.9%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,19 +14,15 @@
  */
 
 var lowestCommonAncestor = function (root, p, q) {
-    while (root) {
-        if (p.val < root.val && q.val < root.val) {
-            // p, q가 모두 현재 노드의 왼쪽에 있음 -> 왼쪽으로 이동
-            root = root.left;
-        } else if (p.val > root.val && q.val > root.val) {
-            // p, q가 모두 현재 노드의 오른쪽에 있음 -> 오른쪽으로 이동
-            root = root.right;
-        } else {
-            // p, q가 다른 방향에 있거나 하나가 root와 같은 경우 -> 현재 root가 LCA
-            return root;
-        }
+     if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
     }
-    return null;
+    if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+
+    return root
 };
   //공통부모는 무조건 존재함
   //첫번째 방법은 두번의 재귀 호출 사용 -> 한번의 탐색만으로도 가능
+  
