dab466d585b5bc88c31729f5a17dc768e23b8eb6
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-03-09 22:01:08 +0900

    Time: 108 ms (2.16%), Space: 71.2 MB (5.1%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let nodeStore = []
+    let cur = head
+
+    while (cur) {
+        nodeStore.push(cur)
+        cur = cur.next
+    }
+    console.log(nodeStore)
+
+    nodeStore.reverse()
+
+    let newHead = nodeStore[0]
+    let newCur = newHead
+
+    for (let i = 1; i < nodeStore.length; i++) {
+        newCur.next = nodeStore[i]
+        newCur=newCur.next
+        if(i===nodeStore.length-1){
+            newCur.next=null
+        }
+    }
+
+    if(!newHead) {
+        return head
+    }
+    return newHead
+
+};
